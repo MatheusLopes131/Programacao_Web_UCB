289a994811451fd911e905f559d3076f0f7fbc5f
MatheusLopes131 <matlopes05@gmail.com>
2025-03-18 10:04:50 -0300

    att atividade2

--- atividade2/matrizTransposta.js
+++ atividade2/matrizTransposta.js
@@ -4,6 +4,18 @@ let matriz = [
     [5,6]
 ];
 
+function imprimirMatriz(A){
+    for(let i = 0; i < A.length; i++){
+        let linha = "";
+        for(let j = 0; j< A[i].length; j++){
+            linha += A[i][j] + " "; // usar \t para ter mais espaço //
+        }
+        console.log(linha);
+    }
+}
+
+imprimirMatriz(matriz);
+
 function transporMatriz(A){
     for(let i = 0; i < A[i].length; i++){
         let linha = "";
