f66c7d1d4dec58d7414f74252b872783c3ab6330
MatheusLopes131 <matlopes05@gmail.com>
2025-03-25 15:16:53 -0300

    add atividade3

--- app_calculadora/index.js
+++ app_calculadora/index.js
@@ -11,6 +11,22 @@ app.get('/ola/:nome', (req,res)=>{
     res.send(`Olá, ${req.params.nome}!`);
 });
 
+app.get('/somar/:a/:b', (req,res)=>{
+    res.send(`O valor de a + b é , ${calc.somar(parseFloat(req.params.a),parseFloat(req.params.b))}!`);
+});
+
+app.get('/subtrair/:a/:b', (req,res)=>{
+    res.send(`O valor de a - b é , ${calc.subtrair(parseFloat(req.params.a),parseFloat(req.params.b))}!`);
+});
+
+app.get('/multiplicar/:a/:b', (req,res)=>{
+    res.send(`O valor de a x b é , ${calc.multiplicar(parseFloat(req.params.a),parseFloat(req.params.b))}!`);
+});
+
+app.get('/dividir/:a/:b', (req,res)=>{
+    res.send(`O valor de a % b é , ${calc.dividir(parseFloat(req.params.a),parseFloat(req.params.b))}!`);
+});
+
 const PORT = 8080;
 app.listen(PORT, ()=>{
     console.log('app rodando na porta ' , PORT);
